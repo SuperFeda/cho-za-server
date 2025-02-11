8c5b710bf0fba2b0ab96da50c81d38404a515830
SuperFeda <112332753+SuperFeda@users.noreply.github.com>
2025-02-11 17:22:54 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 addEventListener("DOMContentLoaded", () => {
-    document.getElementById("burger-menu").addEventListener("click", () => {
-        document.getElementById("header-navbar").classList.toggle("active")
-        document.getElementById("adapt-btns").classList.toggle("active")
-    })
-
     document.getElementById("lang-btn").addEventListener("click", () => {
         document.getElementById("lang-options").classList.toggle("no-display")
     })
